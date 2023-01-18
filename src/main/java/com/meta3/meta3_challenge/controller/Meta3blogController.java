8539c67330163a89ab62db15d7f0d891440215f0
Lucas Mendes <lucas.a.mendes97@hotmail.com>
2023-01-17 22:28:00 -0300

    postDetails

--- src/main/java/com/meta3/meta3_challenge/controller/Meta3blogController.java
+++ src/main/java/com/meta3/meta3_challenge/controller/Meta3blogController.java
@@ -4,6 +4,7 @@ import com.meta3.meta3_challenge.model.Post;
 import com.meta3.meta3_challenge.service.Meta3blogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,6 +22,15 @@ public class Meta3blogController {
         ModelAndView mv = new ModelAndView("posts");
         List<Post> posts = meta3blogService.findAll();
         mv.addObject("posts", posts);
+
+        return mv;
+    }
+
+    @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+    public ModelAndView getPostDetails(@PathVariable("id") long id) {
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post post = meta3blogService.findById(id);
+        mv.addObject("post", post);
 
         return mv;
     }
